0fc97a3c3c35619d8ba5a565d4a3b7e336196c7c
Xancake <xanylein@web.de>
2016-09-25 20:25:25 +0200

    Rotationsknoten korrigiert
    - Um eine Rotationsmatrix zu berechnen wird scheinbar ein radial-Wert (0
    - PI) benötigt. Da wir den Wert selbst als Winkel (Grad) angeben, muss
    der noch in den radial-Wert umgerechnet werden.

--- BAI6_WP_CG_Praktikum/src/main/java/computergraphics/framework/scenegraph/nodes/inner/RotationNode.java
+++ BAI6_WP_CG_Praktikum/src/main/java/computergraphics/framework/scenegraph/nodes/inner/RotationNode.java
@@ -1,6 +1,8 @@
 package computergraphics.framework.scenegraph.nodes.inner;
 
 import com.jogamp.opengl.GL2;
+
+import computergraphics.framework.math.MathHelpers;
 import computergraphics.framework.math.Matrix;
 import computergraphics.framework.math.Vector;
 
@@ -8,7 +10,7 @@ public class RotationNode extends InnerNode {
 	private final Matrix _rotation;
 	
 	public RotationNode(Vector axis, double angle) {
-		_rotation = Matrix.getRotationMatrix4(axis, angle);
+		_rotation = Matrix.getRotationMatrix4(axis, MathHelpers.degree2radiens(angle));
 	}
 	
 	public void traverse(GL2 gl, RenderMode mode, Matrix modelMatrix) {
