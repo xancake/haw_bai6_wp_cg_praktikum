2bab93ba36c2db11a25fdeab85c90ae92e61803f
Xancake <xanylein@web.de>
2016-10-01 17:26:32 +0200

    Formatierung

--- BAI6_WP_CG_Praktikum/src/main/java/computergraphics/framework/mesh/ITriangleMesh.java
+++ BAI6_WP_CG_Praktikum/src/main/java/computergraphics/framework/mesh/ITriangleMesh.java
@@ -15,63 +15,64 @@ import computergraphics.framework.rendering.Texture;
  * @author Philipp Jenke
  */
 public interface ITriangleMesh {
-  /**
-   * Add a new vertex (given by position) to the vertex list. The new vertex is
-   * appended to the end of the list.
-   */
-  public int addVertex(Vector position);
+	/**
+	 * Add a new vertex (given by position) to the vertex list. The new vertex is appended to the end of the list.
+	 */
+	public int addVertex(Vector position);
 
-  /**
-   * Add a new triangle to the mesh with the vertex indices a, b, c. The index
-   * of the first vertex is 0.
-   */
-  public void addTriangle(int vertexIndex1, int vertexIndex2, int vertexIndex3);
+	/**
+	 * Add a new triangle to the mesh with the vertex indices a, b, c. The index of the first vertex is 0.
+	 */
+	public void addTriangle(int vertexIndex1, int vertexIndex2, int vertexIndex3);
 
-  /**
-   * Add triangle by vertex indices and corresponding texture coordinate
-   * indices.
-   */
-  public void addTriangle(int vertexIndex1, int vertexIndex2, int vertexIndex3,
-      int texCoordIndex1, int texCoordIndex2, int texCoordIndex3);
+	/**
+	 * Add triangle by vertex indices and corresponding texture coordinate indices.
+	 */
+	public void addTriangle(
+			int vertexIndex1,
+			int vertexIndex2,
+			int vertexIndex3,
+			int texCoordIndex1,
+			int texCoordIndex2,
+			int texCoordIndex3);
 
-  /**
-   * Add texture coordinate to mesh.
-   */
-  public void addTextureCoordinate(Vector t);
+	/**
+	 * Add texture coordinate to mesh.
+	 */
+	public void addTextureCoordinate(Vector t);
 
-  /**
-   * Set a texture object for the mesh.
-   */
-  public void setTexture(Texture texture);
+	/**
+	 * Set a texture object for the mesh.
+	 */
+	public void setTexture(Texture texture);
 
-  /**
-   * Clear mesh - remove all triangles and vertices.
-   */
-  public void clear();
+	/**
+	 * Clear mesh - remove all triangles and vertices.
+	 */
+	public void clear();
 
-  public int getNumberOfTriangles();
+	public int getNumberOfTriangles();
 
-  public int getNumberOfVertices();
+	public int getNumberOfVertices();
 
-  public Vertex getVertex(int index);
+	public Vertex getVertex(int index);
 
-  public Triangle getTriangle(int triangleIndex);
+	public Triangle getTriangle(int triangleIndex);
 
-  public Vector getTextureCoordinate(int index);
+	public Vector getTextureCoordinate(int index);
 
-  public Texture getTexture();
+	public Texture getTexture();
 
-  /**
-   * Compute the triangles normals.
-   */
-  public void computeTriangleNormals();
+	/**
+	 * Compute the triangles normals.
+	 */
+	public void computeTriangleNormals();
 
-  /**
-   * Create a mesh of the shadow polygons.
-   * 
-   * lightPosition: Position of the light source. extend: Length of the polygons
-   * shadowPolygonMesh: Result is put in there
-   */
-  public void createShadowPolygons(Vector lightPosition, float extend,
-      ITriangleMesh shadowPolygonMesh);
+	/**
+	 * Create a mesh of the shadow polygons.
+	 * @param lightPosition Position of the light source
+	 * @param extend Length of the polygons
+	 * @param shadowPolygonMesh Result is put in there
+	 */
+	public void createShadowPolygons(Vector lightPosition, float extend, ITriangleMesh shadowPolygonMesh);
 }
--- BAI6_WP_CG_Praktikum/src/main/java/computergraphics/framework/mesh/Vertex.java
+++ BAI6_WP_CG_Praktikum/src/main/java/computergraphics/framework/mesh/Vertex.java
@@ -8,31 +8,30 @@ import computergraphics.framework.math.Vector;
  * @author Philipp Jenke
  */
 public class Vertex {
+	/**
+	 * Vertex position in 3-space.
+	 */
+	private Vector position = new Vector(0, 0, 0);
 
-  /**
-   * Vertex position in 3-space.
-   */
-  private Vector position = new Vector(0, 0, 0);
+	/**
+	 * Vertex normal in 3-space.
+	 */
+	private Vector normal = new Vector(0, 1, 0);
 
-  /**
-   * Vertex normal in 3-space.
-   */
-  private Vector normal = new Vector(0, 1, 0);
+	public Vertex(Vector position) {
+		this(position, new Vector(0, 1, 0));
+	}
 
-  public Vertex(Vector position) {
-    this(position, new Vector(0, 1, 0));
-  }
+	public Vertex(Vector position, Vector normal) {
+		this.position.copy(position);
+		this.normal.copy(normal);
+	}
 
-  public Vertex(Vector position, Vector normal) {
-    this.position.copy(position);
-    this.normal.copy(normal);
-  }
+	public Vector getPosition() {
+		return position;
+	}
 
-  public Vector getPosition() {
-    return position;
-  }
-
-  public Vector getNormal() {
-    return normal;
-  }
+	public Vector getNormal() {
+		return normal;
+	}
 }
