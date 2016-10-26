cc8608c5fd06c6517bfac0b30d5460c6e0e4a477
Xancake <xanylein@web.de>
2016-10-26 17:16:30 +0200

    Equals- und hashCode-Methoden für Pair angelegt

--- BAI6_WP_CG_Praktikum/src/main/java/computergraphics/framework/datastructures/Pair.java
+++ BAI6_WP_CG_Praktikum/src/main/java/computergraphics/framework/datastructures/Pair.java
@@ -18,4 +18,42 @@ public class Pair<K, V> {
 	public V getValue() {
 		return _value;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((_key == null) ? 0 : _key.hashCode());
+		result = prime * result + ((_value == null) ? 0 : _value.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(obj == null) {
+			return false;
+		}
+		if(!(obj instanceof Pair)) {
+			return false;
+		}
+		Pair<?, ?> other = (Pair<?, ?>)obj;
+		if(_key == null) {
+			if(other._key != null) {
+				return false;
+			}
+		} else if(!_key.equals(other._key)) {
+			return false;
+		}
+		if(_value == null) {
+			if(other._value != null) {
+				return false;
+			}
+		} else if(!_value.equals(other._value)) {
+			return false;
+		}
+		return true;
+	}
 }
