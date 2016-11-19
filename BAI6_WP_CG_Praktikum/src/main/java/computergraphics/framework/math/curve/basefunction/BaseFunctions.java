b428e90fdc2df3c3030b14e78f329626c66d8eb2
Rokurou-lmb <rokurou.lmb.github@gmail.com>
2016-11-19 15:47:34 +0100

    Praktikum 5:
    - Fehler in der Erzeugung der Hermite-Basisfunktionen behoben

--- BAI6_WP_CG_Praktikum/src/main/java/computergraphics/framework/math/curve/basefunction/BaseFunctions.java
+++ BAI6_WP_CG_Praktikum/src/main/java/computergraphics/framework/math/curve/basefunction/BaseFunctions.java
@@ -28,7 +28,7 @@ public final class BaseFunctions {
 	public static List<BaseFunction> createHermiteFunctions3() {
 		BaseFunction h0 = (t) -> Math.pow(1-t, 2) * (1+2*t);
 		BaseFunction h1 = (t) -> t*Math.pow(1-t, 2);
-		BaseFunction h2 = (t) -> Math.pow(-t, 2) * (1-t);
+		BaseFunction h2 = (t) -> -Math.pow(t, 2) * (1-t);
 		BaseFunction h3 = (t) -> (3-2*t) * Math.pow(t, 2);
 		return Arrays.asList(h0, h1, h2, h3);
 	}
